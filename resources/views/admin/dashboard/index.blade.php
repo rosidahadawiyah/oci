302d9469fa2752659c7e6787babb1a225aa9ea7d
rosidah Adawiyah <rsdhadawiyah@gmail.com>
2024-02-03 10:51:11 +0700

    first commit

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.layout')
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <h3> Selamat datang di halaman dashboard admin.</h3>
+    </div>
+@endsection
---
+++ resources/views/admin/layout.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('admin.partials._head')
+
+<body class="hold-transition sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+  <!-- Navbar -->
+  @include('admin.partials._navbar')
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+ @include('admin.partials._sidebar')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Blank Page</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Blank Page</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      @yield('content')
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  @include('admin.partials._footer')
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+
+@include('admin.partials._scripts')
+</body>
+</html>
